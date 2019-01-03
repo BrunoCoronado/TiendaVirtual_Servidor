8b8754ec872b54643f7f4225e9a90ee4439d9455
Cris6cas <37260651+Cris6cas@users.noreply.github.com>
2019-01-03 16:40:09 -0600

    Create Producto.java

---
+++ src/TDA/TablaHash/Producto.java
@@ -0,0 +1,17 @@
+public class Producto {
+    
+    String codigo;
+    String nombre;
+    String marca;
+    int precio;
+    String ruta;
+    
+    public Producto(String codigo_, String nombre_, String marca, int precio, String ruta){
+        this.codigo=codigo_;
+        this.nombre=nombre_;
+        this.marca=marca;
+        this.precio=precio;
+        this.ruta=ruta;
+    }
+    
+}
